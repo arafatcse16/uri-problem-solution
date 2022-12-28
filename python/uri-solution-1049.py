714cbff561050984e651ab94c353a2092778e976
MD  Arafat Hossain <98521320+arafatcse16@users.noreply.github.com>
2022-12-28 18:10:18 +0600

    Create uri-solution-1049.py

---
+++ python/uri-solution-1049.py
@@ -0,0 +1,19 @@
+x=input()
+y=input()
+z=input()
+if (x=='vertebrado' and y=='ave' and z=='carnivoro'):
+    print("aguia")
+elif(x=='vertebrado' and y=='ave' and z=='onivoro'):
+    print("pomba")
+elif(x=='vertebrado' and y=='mamifero' and z=='onivoro'):
+    print("homem")
+elif(x=='vertebrado' and y=='mamifero' and z=='herbivoro'):
+    print("vaca")
+elif(x=='invertebrado' and y=='inseto' and z=='hematofago'):
+    print("pulga")
+elif(x=='invertebrado' and y=='inseto' and z=='herbivoro'):
+    print("lagarta")
+elif(x=='invertebrado' and y=='anelideo' and z=='hematofago'):
+    print("sanguessuga")
+elif(x=='invertebrado' and y=='anelideo' and z=='onivoro'):
+    print("minhoca")
