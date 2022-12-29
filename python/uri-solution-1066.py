3d27c8a2d41f271ea5dcca1ce6ef1a59a471b0d5
MD  Arafat Hossain <98521320+arafatcse16@users.noreply.github.com>
2022-12-30 00:19:03 +0600

    Update uri-solution-1066.py

--- python/uri-solution-1066.py
+++ python/uri-solution-1066.py
@@ -1,25 +1,18 @@
-even,odd,postive,negative=0,0,0,0
+even=0
+odd=0
+positive=0
+negative=0
 for i in range(5):
-    n=int(input())
-    if n==0:
-        if n%2==0:
-            even+=1
-        else:
-            odd+=1
-    elif n>0:
-        postive+=1
-        if n%2==0:
-            even+=1
-        else:
-            odd+=1
-    elif n<0:
-        negative+=1
-        if n%2==0:
-            even+=1
-        else:
-            odd+=1
-            
+    n = float(input())
+    if(n % 2 == 0):
+         even = even+1
+    else:
+        odd =odd+1
+    if (n > 0):
+        positive =positive+1
+    elif (n < 0):
+        negative =negative+1
 print("{} valor(es) par(es)".format(even))
 print("{} valor(es) impar(es)".format(odd))
-print("{} valor(es) positivo(s)".format(postive))
+print("{} valor(es) positivo(s)".format(positive))
 print("{} valor(es) negativo(s)".format(negative))
